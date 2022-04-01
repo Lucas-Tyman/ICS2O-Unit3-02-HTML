25efa0b280622873f641c2d66a9b54ba6390de4b
Lucas-Tyman <99354994+Lucas-Tyman@users.noreply.github.com>
2022-04-01 17:19:27 +0000

    everything

--- js/script.js
+++ js/script.js
@@ -6,4 +6,15 @@
 /**
  * This function calculates 
  */
-function calculate()
+function calculate() {
+  // input
+  const length = parseFloat(document.getElementById('length-of-pyramid').value)
+  const width = parseFloat(document.getElementById('width-of-pyramid').value)
+  const height = parseFloat(document.getElementById('height-of-pyramid').value)
+
+  // process
+  const volume = (length * width * height) / 3
+  
+  // output
+  document.getElementById('volume').innerHTML = 'Volume is: ' + volume + ' cm³'
+}
